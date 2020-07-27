52f92dc97b94e2b909eee997f98f153892d48877
MARTIN <m2ksoftiez@gmail.com>
2020-07-27 18:30:59 +0200

    Tutorial9

---
+++ src/Tutorial9.kt
@@ -0,0 +1,13 @@
+fun main(){
+
+    val title = "The Amaze Apples"
+    val slogan = "The best apples of all time"
+
+    println(title + ". " + slogan)
+
+    //the wrong version
+    println("title. slogan")
+
+    println("$title. $slogan")
+
+ }
