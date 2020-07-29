9333ec7191a3c1419f12a09409f6d822a950b4f0
MARTIN <m2ksoftiez@gmail.com>
2020-07-29 22:19:07 +0200

    Tutorial9

--- src/Tutorial9.kt
+++ src/Tutorial9.kt
@@ -2,12 +2,11 @@ fun main(){
 
     val title = "The Amaze Apples"
     val slogan = "The best apples of all time"
+    val soldAlready = 20000000
 
-    println(title + ". " + slogan)
+    println(title + ". " + slogan + ". We have sold " + soldAlready +" apples already.")
 
-    //the wrong version
-    println("title. slogan")
+    val saying = "$title. $slogan . We have sold $soldAlready apples already."
 
-    println("$title. $slogan")
-
- }
+    println(saying)
+}
