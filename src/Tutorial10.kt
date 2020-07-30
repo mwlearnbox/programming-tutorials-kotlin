94bed1aa5065ecf410eae4ec70aa1565858bdd0e
MARTIN <m2ksoftiez@gmail.com>
2020-07-30 02:23:23 +0200

    Tutorial10.kt Tutorial11.kt

---
+++ src/Tutorial10.kt
@@ -0,0 +1,7 @@
+fun main(){
+    val title = "The Amaze Apples"
+    val price = 250
+    val sold = 1004
+
+    println("$title have sold $sold apples at $$price giving them a profit of $${price * sold}")
+}
---
+++ src/Tutorial11.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    // \" Double Quote
+
+    // \n New Line
+
+    // \t Tab
+
+    val title = "The Amaze Apples"
+    println("$title Business Sales Conversation")
+    println("\tJimmy: How much are you're Amaze Apples?")
+    println("\tDid that guy just say \"Amaze Apples\"?\n\tYeah man they been selling Amaze Apples across the street man")
+    println("Salesman: £250 it's almost free!")
+    println("Jimmy: Can I buy half an apple for £3")
+    println("Salesman: We don't usually do this, but okay. Meet me by the alleyway")
+    println("\tHow is this company still in business?")
+
+}
