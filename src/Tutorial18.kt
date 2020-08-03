03dea2f47e140aefcf5c01010313eb2aa70f1d01
MARTIN <m2ksoftiez@gmail.com>
2020-08-03 15:00:35 +0200

    Tutorial 18

--- src/Tutorial18.kt
+++ src/Tutorial18.kt
@@ -1,2 +1,19 @@
+fun main(){
 
+    val title = "The Amaze Apples High shool"
 
+    val grade = 100
+
+    if(grade == 100){
+        println("Perfect! Score!")
+    }else if(grade > 80){
+        println("Very Good!")
+    }else if(grade > 60){
+        println("Average!")
+    }else if(grade >= 50){
+        println("Made it! Barely.")
+    }else{
+        println("You failed.")
+    }
+
+}
